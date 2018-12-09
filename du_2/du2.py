49575688d0d6ecf7f6b36b49466eb0f593e3dc53
lnenicp <petra.lnenickova@gmail.com>
2018-12-09 22:41:04 +0100

    sort features

--- du_2/du2.py
+++ du_2/du2.py
@@ -4,6 +4,7 @@ import geojson
 input = 'vstup.geojson'
 output ='vystup.geojson'
 
+property_name = 'cluster_id'
 
 def calculate_bbox(features):
     min_x = float('inf')
@@ -25,15 +26,56 @@ def calculate_bbox(features):
             max_y = point_y
     return min_x, min_y, max_x, max_y
 
+def get_half_value(min_value, max_value):
+    return (min_value + max_value) / 2
+
+def sort_features(features, half_x, half_y):
+    '''
+    Rozdeli vstupni data do skupin dle kvadrantu, do kterych prislusi.
+    (Pomoci prideleni hodnoty do cluster_id)
+    :param features: vstupni data
+    :param half_x: hodnota osy x, podle ktere se bude delit
+    :param half_y: hodnota osy y, podle ktere se bude delit
+    :return: množiny/listy bodu podle kvadrantu
+    '''
+    features1 = []
+    features2 = []
+    features3 = []
+    features4 = []
+    for feature in features:
+        # vlozeni "cluster_id", vytvoreni prazdneho str
+        if property_name not in feature['properties']:
+            feature['properties'][property_name] = ''
+        coordinates = feature['geometry']['coordinates']
+        x = coordinates[0]
+        y = coordinates[1]
+        if x < half_x and y > half_y:
+            feature['properties'][property_name] += '1'
+            features1.append(feature)
+        elif x > half_x and y > half_y:
+            feature['properties'][property_name] += '2'
+            features2.append(feature)
+        elif x < half_x and y < half_y:
+            feature['properties'][property_name] += '3'
+            features3.append(feature)
+        elif x > half_x and y < half_y:
+            feature['properties'][property_name] += '4'
+            features4.append(feature)
+    return features1, features2, features3, features4
+
 # nacteni dat
 with open(input, encoding="utf-8") as json_data:
     data = geojson.load(json_data)
 
+features = input_json.pop('features')
+
+'''
 # pridani nove properties 'qt_index' a zapis jeji hodnoty do vystupniho souboru
 for i in range(len(data['features'])):
     inx =  1
     data['features'][i]['properties']['qt_index'] = inx
-    with open(output, mode="w") as f:
-        geojson.dump(data, f)
+'''
+with open(output, mode="w") as f:
+    geojson.dump(data, f)
 
 
