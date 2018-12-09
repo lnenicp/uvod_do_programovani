67d1a94cf9b93a8220edc31a918544dd2f63afb7
lnenicp <petra.lnenickova@gmail.com>
2018-12-09 10:35:46 +0100

    bounding box

--- du_2/du2.py
+++ du_2/du2.py
@@ -4,6 +4,27 @@ import geojson
 input = 'vstup.geojson'
 output ='vystup.geojson'
 
+
+def calculate_bbox(features):
+    min_x = float('inf')
+    min_y = float('inf')
+    max_x = float('-inf')
+    max_y = float('-inf')
+
+    for feature in features:
+        coordinates = feature['geometry']['coordinates']
+        point_x = coordinates[0]
+        point_y = coordinates[1]
+        if point_x < min_x:
+            min_x = point_x
+        if point_x > max_x:
+            max_x = point_x
+        if point_y < min_y:
+            min_y = point_y
+        if point_y > max_y:
+            max_y = point_y
+    return min_x, min_y, max_x, max_y
+
 # nacteni dat
 with open(input, encoding="utf-8") as json_data:
     data = geojson.load(json_data)
