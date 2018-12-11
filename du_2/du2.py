9a5385c39a06c5fe5548417d012298c6c3d379c4
lnenicp <petra.lnenickova@gmail.com>
2018-12-11 22:24:48 +0100

    komentare

--- du_2/du2.py
+++ du_2/du2.py
@@ -1,13 +1,17 @@
 import geojson
 
-# vtup/vystup
+# vstup/vystup
 INPUT_FILENAME = 'input.geojson'
 OUTPUT_FILENAME = 'output.geojson'
 MAX_FEATURES = 50
 
-PROPERTY_NAME = 'cluster_id'
 
 def calculate_bbox(features):
+    '''
+    Vypočítá ohraničující obdélník pro vstupní bodová data.
+    :param features: vstupní data (features vstupního GeoJsonu)
+    :return: souřanice: min_x, min_y, max_x, max_y
+    '''
     min_x = float('inf')
     min_y = float('inf')
     max_x = float('-inf')
@@ -28,43 +32,64 @@ def calculate_bbox(features):
     return min_x, min_y, max_x, max_y
 
 def get_half_value(min_value, max_value):
+    '''
+    Určí střed mezi minimální a maximalní hodnotou.
+    (Využito pro hledání os, podle kterých probíhá dělení do kvadrantů.)
+    :param min_value: minimální hodnota/souřadnice na dané ose
+    :param max_value: maximální hodnota/souřadnice na dané ose
+    :return: "průměr" ze zadaných hodnot
+    '''
     return (min_value + max_value) / 2
 
 def sort_features(features, half_x, half_y):
     '''
-    Rozdeli vstupni data do skupin dle kvadrantu, do kterych prislusi.
-    (Pomoci prideleni hodnoty do cluster_id)
-    :param features: vstupni data
-    :param half_x: hodnota osy x, podle ktere se bude delit
-    :param half_y: hodnota osy y, podle ktere se bude delit
-    :return: množiny/listy bodu podle kvadrantu
+    Rozdělí vstupní data do skupin dle kvadrantu, do kterých přísluší. Přidá se nové properties - 'cluster_id'.
+    Zároveň se při tom načte/přidá hodnota příslušného kvadrantu do properties - cluster_id.
+    :param features: vstupní data (features vstupního GeoJsonu)
+    :param half_x: hodnota osy x, podle které se budou dělit body do kvadrantů
+    :param half_y: hodnota osy y, podle které se budou dělit body do kvadrantů
+    :return: množiny/listy bodu podle kvadrantu: features1, features2, features3, features4
     '''
     features1 = []
     features2 = []
     features3 = []
     features4 = []
     for feature in features:
-        # vlozeni "cluster_id", vytvoreni prazdneho str
-        if PROPERTY_NAME not in feature['properties']:
-            feature['properties'][PROPERTY_NAME] = ''
+        if 'cluster_id' not in feature['properties']:
+            feature['properties']['cluster_id'] = ''
         coordinates = feature['geometry']['coordinates']
         x = coordinates[0]
         y = coordinates[1]
         if x < half_x and y > half_y:
-            feature['properties'][PROPERTY_NAME] += '1'
+            feature['properties']['cluster_id'] += '1'
             features1.append(feature)
         elif x > half_x and y > half_y:
-            feature['properties'][PROPERTY_NAME] += '2'
+            feature['properties']['cluster_id'] += '2'
             features2.append(feature)
         elif x < half_x and y < half_y:
-            feature['properties'][PROPERTY_NAME] += '3'
+            feature['properties']['cluster_id'] += '3'
             features3.append(feature)
         elif x > half_x and y < half_y:
-            feature['properties'][PROPERTY_NAME] += '4'
+            feature['properties']['cluster_id'] += '4'
             features4.append(feature)
     return features1, features2, features3, features4
 
-def quadtree(input_features, output_json, min_x, min_y, max_x, max_y):
+def quadtree(input_features, output_json, MAX_FEATURES, min_x, min_y, max_x, max_y):
+    '''
+    Určuje příslušnost vstupních bodů do konkréních clusterů.
+    Nejdříve vypočítá "souřadnice os", podle kterých body rozdělí dle kvadrantů. Rozdělí body do kvadrantů.
+    Zároveň se id příslušného kvadrantu načte do properties-cluster_id. (Při dalším/opakovaném dělení se
+    id "přidá" za již napsanou hodnotu).
+    Pokud se v příslušném clusteru nachází více než maximální povolené množství bodů (MAX_FEATURES),
+    cluster se bude dělit opakovaně znovu, dokud nebude tato podmínka splněna.
+    :param input_features: vstupní data (features vstupního GeoJsonu)
+    :param output_json: výstupní data (features výstupního GeoJsonu)
+    :param min_x: minimální souřadnice na ose x
+    :param min_y: minimální souřadnice na ose y
+    :param max_x: maximální souřadnice na ose x
+    :param max_y: maximální souřadnice na ose y
+    :return: id příslušného kvadrantu se zapíše/připíše do properties-cluster_id
+    '''
     if len(input_features) > MAX_FEATURES:
 
         # vypocte osy, podle kterych se bude delit
@@ -78,23 +103,23 @@ def quadtree(input_features, output_json, min_x, min_y, max_x, max_y):
         # funkce se vola opakovane pro vsechny kvadranty a body se dale/opakovane deli
         # minima a maxima se predefinuji pro kazdy kvadrant
         quadtree(
-            features1, output_json,
+            features1, output_json, MAX_FEATURES,
             min_x=min_x, min_y=half_y, max_x=half_x, max_y=max_y
         )
         quadtree(
-            features2, output_json,
+            features2, output_json, MAX_FEATURES,
             min_x=half_x, min_y=half_y, max_x=max_x, max_y=max_y
         )
         quadtree(
-            features3, output_json,
+            features3, output_json, MAX_FEATURES,
             min_x=min_x, min_y=min_y, max_x=half_x, max_y=half_y
         )
         quadtree(
-            features4, output_json,
+            features4, output_json, MAX_FEATURES,
             min_x=half_x, min_y=min_y, max_x=max_x, max_y=half_y
         )
     else:
-        print(f'less than "{MAX_FEATURES}" features, saving to output')
+        # v kvadrantu je mene nez zvoleny pocet prvku, dalsi deleni neprobiha
         output_json['features'] += input_features
 
 
@@ -107,7 +132,7 @@ def run():
     # vyjme "část" s fetaures
     input_features = input_json.pop('features')
 
-    # přepřipravení výstupního souboru - ostatní klíče a hodnoty krome features
+    # předpřipravení výstupního souboru - ostatní klíče a hodnoty krome features
     output_json = input_json
     # připravení prázdného listu "feautures" pro výstupní soubor
     output_json['features'] = []
@@ -115,12 +140,13 @@ def run():
     # výpočet ohraničujícího obdélníku
     min_x, min_y, max_x, max_y = calculate_bbox(input_features)
 
-    # trideni bodu do kvadratu (soucasne tvorba "cluster_id"
-    quadtree(input_features, output_json,min_x=min_x, min_y=min_y, max_x=max_x, max_y=max_y)
+    # trideni bodu dle kvadratu, zapis hodnot cluster_id
+    quadtree(input_features, output_json, MAX_FEATURES, min_x=min_x, min_y=min_y, max_x=max_x, max_y=max_y)
 
     # finální zápis dat
     with open(OUTPUT_FILENAME, 'w') as output_geojson:
         geojson.dump(output_json, output_geojson)
 
-
 run()
+
+
